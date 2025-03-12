c4153efd7046ab5549aebabca1ce7ccc42df304d
gitethanwoo <strictlyethan@gmail.com>
2025-03-12 02:05:02 -0500

    added responses web search tool

--- lib/generate-response.ts
+++ lib/generate-response.ts
@@ -1,12 +1,25 @@
-import { CoreMessage } from "ai";
+import { openai } from '@ai-sdk/openai';
+import { CoreMessage, generateText, tool } from 'ai';
+import { z } from 'zod';
+
 
 export const generateResponse = async (
   messages: CoreMessage[],
   updateStatus?: (status: string) => void,
 ) => {
-  let text = "";
-  // TODO: call llm here
+  const { text } = await generateText({
+    model: openai.responses('gpt-4o'),
+    system: `You are a Slack bot assistant. Keep your responses concise and to the point.
+    - Do not tag users.
+    - Current date is: ${new Date().toISOString().split('T')[0]}
+    - Always include sources in your final response if you use web search.`,
+    messages,
+    maxSteps: 10,
+    tools: {
+      web_search_preview: openai.tools.webSearchPreview(),
+    },
+  });
 
   // Convert markdown to Slack mrkdwn format
-  return text.replace(/\[(.*?)\]\((.*?)\)/g, "<$2|$1>").replace(/\*\*/g, "*");
+  return text.replace(/\[(.*?)\]\((.*?)\)/g, '<$2|$1>').replace(/\*\*/g, '*');
 };
